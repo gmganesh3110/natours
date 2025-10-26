f0fc8eb09d110b38402b2c4d624cdc02435338cc
G M Ganesh <ganesh.gm@magnificenztech.com>
2025-10-26 20:19:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+const cors=require('cors');
+
+dotenv.config({path:'./.env'});
+
+const app=express();
+
+// MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+// ROUTES
+app.get('/',(req,res)=>{
+    res.send('Natours API is running');
+});
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
