8c506c879335b0ce39fee2c730b8957c18cbc660
G M Ganesh <ganesh.gm@magnificenztech.com>
2025-10-31 11:14:49 +0530

    Refactor index.js for improved readability and error handling; added error handling middleware and formatted code for consistency.

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-const express=require('express');
-const morgan=require('morgan');
-const dotenv=require('dotenv');
-const cors=require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-dotenv.config({path:'./.env'});
+dotenv.config({ path: "./.env" });
 
-const tourRoutes=require('./routes/tourRoutes')
+const tourRoutes = require("./routes/tourRoutes");
 
-const app=express();
+const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
 // DATABASE CONNECTION
-const connection=require('./database/connection');
+const connection = require("./database/connection");
 connection();
 
 // ROUTES
-app.get('/',(req,res)=>{
-    res.send('Natours API is running');
+app.get("/", (req, res) => {
+  res.send("Natours API is running");
 });
 
+app.use("/api/tour", tourRoutes);
 
-app.use("/api/tour",tourRoutes)
+app.use((req, res,next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
 
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
 
-const PORT=process.env.PORT || 3000;
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    messsage: err.message,
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
